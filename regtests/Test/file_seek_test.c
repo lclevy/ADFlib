37251747d70a1c2f7b8478b5551668b4332ac00d
t-w <tomas.wolak@gmail.com>
2022-11-22 11:25:38 +0100

    File seek test: refactored (simplified and easier to expand).

--- regtests/Test/file_seek_test.c
+++ regtests/Test/file_seek_test.c
@@ -6,13 +6,63 @@
 #include"adflib.h"
 
 
-int test_reading_data_after_seek ( struct Volume * const vol,
-                                   char *                filename,
-                                   unsigned int          offset,
-                                   const unsigned  char  expected_value );
+typedef struct check_s {
+    unsigned int  offset;
+    unsigned char value;
+} check_t;
 
-int test_reading_data_after_seek_ofs (  char * adf_image_fname );
-int test_reading_data_after_seek_ffs (  char * adf_image_fname );
+typedef struct reading_test_s {
+    char * image_filename;
+    char * filename;
+
+    unsigned int nchecks;
+    check_t      checks[];
+} reading_test_t;
+
+
+const unsigned int ofs_highest_pos_not_using_extension = 35135;  // 0x893f
+
+reading_test_t test_ofs = {
+    .filename = "moon.gif",
+    .nchecks = 7,
+    .checks = { { 0, 0x47 },
+                { 1, 0x49 },
+                { ofs_highest_pos_not_using_extension, 5 },
+                { ofs_highest_pos_not_using_extension + 1, 0x2d },
+                { ofs_highest_pos_not_using_extension + 2, 0x6c },
+                { 0x2a708, 3 },
+                { 0x2a716, 0x3b }  // the last byte of the file
+                // add checking beyond the file ?
+    }
+};
+
+
+const unsigned int ffs_highest_pos_not_using_extension = //39935;  // 0x9bff   ( 512 * 72) ?
+    36863;  // 0x9bff
+
+reading_test_t test_ffs = {
+    .filename = "mod.And.DistantCall",
+    .nchecks = 11,
+    .checks = { { 0, 0x64 },
+                { 1, 0x69 },
+                { ofs_highest_pos_not_using_extension, 0x28 },
+                { ofs_highest_pos_not_using_extension + 1, 0x24 },
+                { ofs_highest_pos_not_using_extension + 2, 0x1b },
+                { ffs_highest_pos_not_using_extension, 0xff },
+                { ffs_highest_pos_not_using_extension + 1, 0x07 },
+                { ffs_highest_pos_not_using_extension + 2, 0x0c },
+                { 0x237c0, 0xfc },
+                { 0x237ce, 0xea },
+                { 0x237cf, 0x00 },  // the last byte of the file
+                // add checking beyond the file ?
+    }
+};
+
+
+int test_single_seek ( reading_test_t * test_data );
+int check_single_seek ( struct File *       file,
+                        unsigned int        offset,
+                        const unsigned char expected_value );
 
 
 int main ( int argc, char * argv[] )
@@ -21,8 +71,12 @@ int main ( int argc, char * argv[] )
 
 //	adfSetEnvFct(0,0,MyVer,0);
     int status = 0;
-    status += test_reading_data_after_seek_ofs ( argv[1] );
-    status += test_reading_data_after_seek_ffs ( argv[2] );
+
+    test_ofs.image_filename = argv[1];
+    status += test_single_seek ( &test_ofs );
+
+    test_ffs.image_filename = argv[2];
+    status += test_single_seek ( &test_ffs );
 
     adfEnvCleanUp();
 
@@ -30,207 +84,75 @@ int main ( int argc, char * argv[] )
 }
 
 
-int test_reading_data_after_seek_ofs ( char * adf_image_fname )
+
+int test_single_seek ( reading_test_t * test_data )
 {
-    int status = 0;
+    printf ( "\n*** Test file seeking on image: %s, filename: %s\n",
+             test_data->image_filename, test_data->filename );
 
-    printf ( "\n\n*********** TEST FILE SEEKING ON OFS\n" );
-
-    /* mount an existing device : OFS */
-    struct Device * const dev = adfMountDev ( adf_image_fname, TRUE );
-    struct Volume * const vol = adfMount ( dev, 0, TRUE );
-    adfVolumeInfo ( vol );
-
-
-    /* test file seek OK.*/
-    //const unsigned int highest_not_failing_pos = 36864;
-    const unsigned int highest_not_failing_pos = 35135;  // 0x893f
-    char large_file[] = "moon.gif";
-    struct File * file = adfOpenFile ( vol, large_file, "r" );
-    if ( ! file )  {
-        status = 1;
-        goto cleanup;
-    }
-    printf ( "Seeking to a non-failing position (%d)...\n",
-             highest_not_failing_pos );
-    adfFileSeek ( file, highest_not_failing_pos );
-    adfCloseFile ( file );
-    printf ("OK.\n");
-
-    // test file seek that fail
-    file = adfOpenFile ( vol, large_file, "r" );
-    if ( ! file ) {
-        status = 1;
-        goto cleanup;
-    }
-    printf ( "Seeking to the first failing position (%d)...\n",
-             highest_not_failing_pos + 1 );
-    adfFileSeek ( file, highest_not_failing_pos + 1 ); // SEGFAULT(!)
-    adfCloseFile ( file );
-    printf ("OK.\n");
-
-
-// test reading data after a seek
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             0,
-                                             0x47 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             1,
-                                             0x49 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos,
-                                             0x5 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos + 1,
-                                             0x2d );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos + 2,
-                                             0x6c );
-
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             0x2a708, 3 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             0x2a716, 0x3b );  // the last byte of the file
-
-cleanup:
-    adfUnMount ( vol );
-    adfUnMountDev ( dev );
-
-    return status;
-}
-
-
-int test_reading_data_after_seek_ffs ( char * adf_image_fname )
-{
-    int status = 0;
-
-    printf ( "\n\n*********** TEST FILE SEEKING ON FFS\n" );
-
-    /* mount an existing device : OFS */
-    struct Device * const dev = adfMountDev ( adf_image_fname, TRUE );
-    struct Volume * const vol = adfMount ( dev, 0, TRUE );
-    adfVolumeInfo ( vol );
-
-
-    /* test file seek OK.*/
-    //const unsigned int highest_not_failing_pos = 36864;
-    const unsigned int highest_not_failing_pos_ofs = 35135;  // 0x893f
-    //const unsigned int highest_not_failing_pos_ffs = 39935;  // 0x9bff   ( 512 * 72) ?
-    const unsigned int highest_not_failing_pos_ffs = 36863;  // 0x9bff
-    char large_file[] = "mod.And.DistantCall";
-    struct File * file = adfOpenFile ( vol, large_file, "r" );
-    if ( ! file )  {
-        status = 1;
-        goto cleanup;
-    }
-    printf ( "Seeking to a non-failing position (%d)...\n",
-             highest_not_failing_pos_ffs );
-    adfFileSeek ( file, highest_not_failing_pos_ffs );
-    adfCloseFile ( file );
-    printf ("OK.\n");
-
-
-    // test file seek that fail
-    file = adfOpenFile ( vol, large_file, "r" );
-    if ( ! file ) {
-        status = 1;
-        goto cleanup;
-    }
-    printf ( "Seeking to the first failing position (%d)...\n",
-             highest_not_failing_pos_ffs + 1 );
-    adfFileSeek ( file, highest_not_failing_pos_ffs + 1 ); // SEGFAULT(!)
-    adfCloseFile ( file );
-    printf ("OK.\n");
-
-
-// test reading data after a seek
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             0,
-                                             0x64 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             1,
-                                             0x69 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos_ofs,
-                                             0x28 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos_ofs + 1,
-                                             0x24 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos_ofs + 2,
-                                             0x1b );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos_ffs,
-                                             0xff );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos_ffs + 1,
-                                             0x07 );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             highest_not_failing_pos_ffs + 2,
-                                             0x0c );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             0x237c0, 0xfc );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             0x237ce, 0xea );
-
-    status += test_reading_data_after_seek ( vol, large_file,
-                                             0x237cf, 0x00 );  // the last byte of the file
-
-cleanup:
-    adfUnMount ( vol );
-    adfUnMountDev ( dev );
-
-    return status;
-}
-
-
-
-// test reading data after a seek
-int test_reading_data_after_seek ( struct Volume * const vol,
-                                   char *                filename,
-                                   unsigned int          offset,
-                                   const unsigned char   expected_value )
-{
-    printf ( "\n  ************************************************************"
-             "\n  *** Test reading data after seek to position 0x%x (%d)...\n",
-             offset, offset );
-
-    struct File * file = adfOpenFile ( vol, filename, "r" );
-    if ( ! file )
+    struct Device * const dev = adfMountDev ( test_data->image_filename, TRUE );
+    if ( ! dev ) {
+        printf ( "Cannot mount image %s - aborting the test...\n",
+                 test_data->image_filename );
         return 1;
+    }
+
+    struct Volume * const vol = adfMount ( dev, 0, TRUE );
+    if ( ! vol ) {
+        printf ( "Cannot mount volume 0 from image %s - aborting the test...\n",
+                 test_data->image_filename );
+        adfUnMountDev ( dev );
+        return 1;
+    }
+    adfVolumeInfo ( vol );
+
+    int status = 0;
+    for ( int i = 0 ; i < test_data->nchecks ; ++i ) {
+
+        struct File * file = adfOpenFile ( vol, test_data->filename, "r" );
+        if ( ! file ) {
+            printf ("Cannot open file %s - aborting...\n", test_data->filename );
+            status = 1;
+            goto cleanup;
+        }
+
+        status += check_single_seek ( file,
+                                      test_data->checks[i].offset,
+                                      test_data->checks[i].value );
+        adfCloseFile ( file );
+    }
+
+cleanup:
+    adfUnMount ( vol );
+    adfUnMountDev ( dev );
+
+    return status;
+}
+
+
+int check_single_seek ( struct File *       file,
+                        unsigned int        offset,
+                        const unsigned char expected_value )
+{
+    printf ( "  Reading data after seek to position 0x%x (%d)...",
+             offset, offset );
 
     adfFileSeek ( file, offset );
 
     unsigned char c;
     int n = adfReadFile ( file, 1, &c );
-    adfCloseFile ( file );
 
     if ( n != 1 ) {
-        printf ( "Reading data failed!\n" );
+        printf ( " -> Reading data failed!!!\n" );
         return 1;
     }
 
     if ( c != expected_value ) {
-        printf ( "      Incorrect data read:  expected 0x%x != read 0x%x\n",
+        printf ( " -> Incorrect data read:  expected 0x%x != read 0x%x\n",
                  (int) expected_value, (int) c );
         return 1;
     }
 
-    printf ( "      OK.\n" );
+    printf ( " -> OK.\n" );
     return 0;
 }
