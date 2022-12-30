8c19350ed59a6be821ebeb5ef6d663521e2d8269
t-w <tomas.wolak@gmail.com>
2022-12-30 17:18:13 +0100

    Examples: adding simple utilities for creating and formatting floppy disk images, lclevy/ADFlib#17

---
+++ examples/adf_floppy_create.c
@@ -0,0 +1,84 @@
+
+#include <adflib.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void usage ( void );
+
+
+int main ( int     argc,
+           char ** argv )
+{
+    if ( argc < 3 ) {
+        usage();
+        return 1;
+    }
+
+    char * const adfname = argv[1];
+
+    FILE * const f = fopen ( adfname, "rb" );
+    if ( f ) {
+        fclose ( f );
+        fprintf ( stderr, "'%s' already exists - aborting...\n", adfname );
+        return 1;
+    }
+    
+    const char * const floppy_type = argv[2];
+    int       tracks               = 80;
+    const int HEADS                = 2;
+
+    int sectors_per_track;
+    if ( strncmp ( floppy_type, "dd", 2 ) == 0 )
+        sectors_per_track = 11;
+    else if ( strncmp ( floppy_type, "hd", 2 ) == 0 ) {
+        sectors_per_track = 22;
+    } else {
+        fprintf ( stderr, "Incorrect fdtype '%s'.\n", floppy_type );
+        return 1;        
+    }
+
+    // postfix to fdtype can specifie non-standard 80+ tracks
+    if ( floppy_type[2] ) {
+        if ( strcmp ( floppy_type + 2, "81" ) == 0 )
+            tracks = 81;
+        else if ( strcmp ( floppy_type + 2, "82" ) == 0 )
+            tracks = 82;
+        else if ( strcmp ( floppy_type + 2, "83" ) == 0 )
+            tracks = 83;
+        else {
+            fprintf ( stderr, "Incorrect fdtype '%s'.\n", floppy_type );
+            return 1;        
+        }
+    }
+    
+    printf ( "Creating floppy disk image: '%s'\n", adfname );
+
+    adfEnvInitDefault();
+
+    struct Device * device = adfCreateDumpDevice ( adfname, tracks, HEADS,
+                                                   sectors_per_track );
+    if ( ! device ) {
+        fprintf ( stderr, "Error creating floppy (%s) disk image %s.\n",
+                  floppy_type, adfname );
+        return 1;
+    }
+    adfDeviceInfo ( device );
+    printf ( "Done!\n" );
+
+    adfUnMountDev ( device );
+    adfEnvCleanUp();
+
+    return 0;
+}
+
+
+void usage ( void )
+{
+    printf ( "Usage:  adf_create_floppy filename fdtype\n\n"
+             "   fdtype can be:\n"
+             "     'dd' -> 880K\n"
+             "     'hd' -> 1760K\n"
+             "     'hd81', 'hd82', 'hd83' (non-standard 81-83 tracks)\n\n" );
+}
---
+++ examples/adf_floppy_format.c
@@ -0,0 +1,91 @@
+
+#include <adflib.h>
+#include "adf_hd.h"
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void usage ( void );
+
+
+int main ( int     argc,
+           char ** argv )
+{
+    if ( argc < 4 ) {
+        usage();
+        return 1;
+    }
+
+    char * adfname = argv[1];
+    char * label   = argv[2];
+
+    errno = 0;
+    unsigned long type = strtoul ( argv[3], NULL, 10 );
+    if ( errno != 0 ) {
+        fprintf ( stderr, "Incorrect fstype %ld.\n", type );
+        return 1;
+    }
+
+    adfEnvInitDefault();
+
+    struct Device * device = adfMountDev ( adfname, FALSE );
+    if ( device ) {
+        fprintf ( stderr, "The floppy disk %s already contains a filesystem - aborting...\n",
+                  adfname );
+        adfUnMountDev ( device );
+        return 1;
+    }
+
+    device = adfOpenDev ( adfname, FALSE );
+    if ( ! device ) {
+        fprintf ( stderr, "Cannot open floppy disk %s - aborting...\n", adfname );
+        return 1;
+    }
+
+    //adfDeviceInfo ( device );
+
+    char *fdtype;
+    int devtype = adfDevType ( device );
+    if ( devtype == DEVTYPE_FLOPDD ) {
+        device->sectors = 11;
+        device->heads   = 2;
+        fdtype          = "DD";
+    } else if ( devtype == DEVTYPE_FLOPHD ) {
+        device->sectors = 22;
+        device->heads   = 2;
+        fdtype          = "HD";
+    } else { //if ( devtype == DEVTYPE_HARDDISK ) {
+        fprintf ( stderr, "The device is not a floppy - aborting...\n" );
+        return 1;
+    }
+    device->cylinders = device->size / ( device->sectors * device->heads * 512 );
+
+    adfDeviceInfo ( device );
+
+    printf ( "Formatting floppy (%s) disk '%s'...\n", fdtype, adfname );
+    if ( adfCreateFlop ( device, label, (unsigned char) type ) != RC_OK ) {
+        fprintf ( stderr, "Error formatting the disk image '%s'!", adfname );
+        adfCloseDev ( device );
+        adfEnvCleanUp();
+        return 1;
+    }
+    printf ( "Done!\n" );
+ 
+    adfCloseDev ( device );
+    adfEnvCleanUp();
+
+    return 0;
+}
+
+
+void usage ( void )
+{
+    printf ( "Usage:  adf_create_floppy filename label fstype\n\n"
+//             " where:\n"
+             "   fstype can be 0-7: flags = 3 least significant bits\n"
+             "         bit  set         clr\n"
+             "         0    FFS         OFS\n"
+             "         1    INTL ONLY   NO_INTL ONLY\n"
+             "         2    DIRC&INTL   NO_DIRC&INTL\n\n" );
+}
