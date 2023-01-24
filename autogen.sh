96827928134b4f028d35f6907731c8144380130a
t-m <tomas.wolak@gmail.com>
2023-01-24 19:56:46 +0100

    autotools / autogen: detect automatically Darwin/MacOSX (and use glibtoolize there).

--- autogen.sh
+++ autogen.sh
@@ -2,11 +2,21 @@
 
 # Some hosts (Mac homebrew) installs a modern libtoolize as 'glibtoolize'.
 # Allow `LIBTOOLIZE' to be set in the environment to allow this.
-if [ "x${LIBTOOLIZE}" = "x" ]
-then
-  LIBTOOLIZE=libtoolize
-fi
-$LIBTOOLIZE --copy --force
+#if [ "x${LIBTOOLIZE}" = "x" ]
+#then
+#  LIBTOOLIZE=libtoolize
+#fi
+#$LIBTOOLIZE --copy --force
+
+#
+# Set glibtoolize on Darwin (ie. MacOSX)
+case `uname` in
+       Darwin*)
+               LIBTOOLIZE=glibtoolize
+               ;;
+       *)
+               LIBTOOLIZE=libtoolize
+esac
 
 aclocal
 autoconf
