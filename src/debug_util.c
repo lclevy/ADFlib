d6625fd458209c41df143f14b3ee29a8507ed2ad
t-m <tomas.wolak@gmail.com>
2023-04-30 22:14:57 +0200

    Debugging: adding an optionally compiled module for misc. util. functions for debugging (only printBacktrace() inside).

---
+++ src/debug_util.c
@@ -0,0 +1,35 @@
+
+#include <execinfo.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "debug_util.h"
+
+
+/*
+  Require link option: -rdynamic
+
+  More info:
+  https://www.gnu.org/software/libc/manual/html_node/Backtraces.html
+  https://stackoverflow.com/questions/5945775/how-to-get-more-detailed-backtrace
+  https://stackoverflow.com/questions/3899870/print-call-stack-in-c-or-c/54365144#54365144
+  https://stackoverflow.com/questions/6934659/how-to-make-backtrace-backtrace-symbols-print-the-function-names
+ */
+void printBacktrace ( void )
+{
+    const unsigned BUFSIZE = 100;
+    void *buffer [ BUFSIZE ];
+
+    int size = backtrace ( buffer, BUFSIZE );
+    const char * const * const strings = backtrace_symbols ( buffer, size );
+    if ( strings == NULL ) {
+        perror ( "error getting symbols" );
+        return;
+    }
+
+    printf ( "Obtained %d stack frames.\n", size );
+    for ( unsigned i = 0 ; i < size ; i++ )
+        printf ( "%s\n", strings[i] );
+
+    free ( strings );
+}
---
+++ src/debug_util.h
@@ -0,0 +1,7 @@
+
+#ifndef __ADFLIB_DEBUG_UTIL__
+#define __ADFLIB_DEBUG_UTIL__
+
+void printBacktrace ( void );
+
+#endif
