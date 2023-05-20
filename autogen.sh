a82fbc7765aa0ff3f239be7ca838de7828334dd1
Stuart Caie <kyzer@cabextract.org.uk>
2023-05-20 10:27:25 +0000

    Use autoreconf to run all autotools
    autoreconf does all the steps that autogen.sh did

--- autogen.sh
+++ autogen.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
 
-# Some hosts (Mac homebrew) installs a modern libtoolize as 'glibtoolize'.
-# Allow `LIBTOOLIZE' to be set in the environment to allow this.
-#if [ "x${LIBTOOLIZE}" = "x" ]
-#then
-#  LIBTOOLIZE=libtoolize
-#fi
-
-
-# Set libtoolize (or glibtoolize on Darwin, ie. MacOSX)
-case `uname` in
-       Darwin*)
-               LIBTOOLIZE=glibtoolize
-               ;;
-       *)
-               LIBTOOLIZE=libtoolize
-esac
-
-$LIBTOOLIZE --copy --force
-aclocal
-autoconf
-autoheader
-automake --add-missing
-
+[ -d m4 ] || mkdir m4
+autoreconf -i -W all
