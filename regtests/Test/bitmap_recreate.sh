bc4ab070c8d808c0f8c747d1b8f2f603edbf770f
t-m <tomas.wolak@gmail.com>
2023-12-01 21:36:35 +0100

    bitmap recreate: make the script work with filenames with whitespaces.

--- regtests/Test/bitmap_recreate.sh
+++ regtests/Test/bitmap_recreate.sh
@@ -7,9 +7,9 @@ set -e
 
 echo "----- bitmap_recreate"
 
-for DUMP_ORIG in `ls $DUMPS/*.adf`
+for DUMP_ORIG in $DUMPS/*.adf
 do
-    DUMP=`basename $DUMP_ORIG`
+    DUMP=`basename "${DUMP_ORIG}"`
 
     # exclude failing
     [ "$DUMP" = "cache_crash.adf" ] && continue;
@@ -17,14 +17,14 @@ do
     [ "$DUMP" = "testhd.adf" ]      && continue;
 
     #DUMP_UPDATED=${DUMP}.bitmap_update.adf
-    echo "Copying $DUMP_ORIG to $DUMP $DUMP_UPDATED"
+    echo "Copying ${DUMP_ORIG} to ${DUMP} ${DUMP_UPDATED}"
     #cat $DUMP_ORIG | tee $DUMP > $DUMP_UPDATED
-    cat $DUMP_ORIG > $DUMP
+    cat "${DUMP_ORIG}" > "${DUMP}"
 
-    ./bitmap_recreate $DUMP
+    ./bitmap_recreate "${DUMP}"
 
     #cmp -l ${DUMP_COPY} $DUMP_UPDATED
 
     #rm -fv $DUMP $DUMP_UPDATED
-    rm -fv $DUMP
+    rm -fv "${DUMP}"
 done
