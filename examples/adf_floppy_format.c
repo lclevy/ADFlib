16ff5b3bc64e38d5218397dcd72785c4ddeb8962
t-m <tomas.wolak@gmail.com>
2024-03-17 13:45:56 +0100

    examples / adf_floppy_format: improve and add new features.
    - Changed / improved command line options (with reasonable defaults).
    - Add possibility to 'enforce' formatting even if a filesystem
      already exists.
    - Allow formatting HDF volumes (hard disk-like large devices
      with floppy-like structure, ie. without the RDSK block,
      without partitions, just one volume on the whole device).

--- examples/adf_floppy_format.c
+++ examples/adf_floppy_format.c
@@ -3,85 +3,143 @@
 #include "adf_dev.h"
 #include "adf_dev_flop.h"
 #include <errno.h>
+#include <libgen.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+typedef struct CmdlineOptions {
+    char     *adfName,
+             *label;
+    unsigned  volidx;
+    uint8_t   fsType;
+    bool      force,
+              verbose,
+              help,
+              version;
+} CmdlineOptions;
 
 
-void usage ( void );
+bool parse_args ( const int * const    argc,
+                  char * const * const argv,
+                  CmdlineOptions *     options );
 
 
-int main ( int     argc,
-           char ** argv )
+void usage ( void )
 {
-    if ( argc < 4 ) {
-        usage();
-        return 1;
+    printf ( "\nUsage:  adf_floppy_format [-f] [-l label] [-p volume] [-t fstype] adf_device\n\n"
+             "Quick-format an ADF (Amiga Disk File) or an HDF (Hard Disk File) volume.\n\n"
+             "Options:\n"
+             "  -f         force formatting even if a filesystem already present\n"
+             "             (WARNING: know what you're doing, irreversible data loss!)\n"
+             "  -l label   set volume name/label, default: ""\n"
+             "  -p volume  volume/partition index, counting from 0, default: 0\n"
+             "  -t fstype  set A. DOS filesystem type (OFS/FFS + INTL, DIR_CACHE)\n"
+             "  -v         be more verbose\n\n"
+             "  -h         show help\n"
+             "  -V         show version\n\n"
+
+             "  fstype can be 0-7: flags = 3 least significant bits\n"
+             "         bit  set         clr\n"
+             "         0    FFS         OFS\n"
+             "         1    INTL ONLY   NO_INTL ONLY\n"
+             "         2    DIRC&INTL   NO_DIRC&INTL\n\n" );
+}
+
+
+int main ( const int            argc,
+           char * const * const argv )
+{
+    CmdlineOptions options;
+    if ( ! parse_args ( &argc, argv, &options ) ) {
+        fprintf ( stderr, "Usage info:  adf_format -h\n" );
+        exit ( EXIT_FAILURE );
     }
 
-    char * adfname = argv[1];
-    char * label   = argv[2];
+    if ( options.help ) {
+        usage();
+        exit ( EXIT_SUCCESS );
+    }
 
-    errno = 0;
-    unsigned long type = strtoul ( argv[3], NULL, 10 );
-    if ( errno != 0 ) {
-        fprintf ( stderr, "Incorrect fstype %ld.\n", type );
-        return 1;
+    if ( options.version ) {
+        printf ( "\n%s (%s), powered by ADFlib %s (%s)\n\n",
+                 ADFLIB_VERSION, ADFLIB_DATE,
+                 adfGetVersionNumber(), adfGetVersionDate() );
+        exit ( EXIT_SUCCESS );
     }
 
     adfEnvInitDefault();
 
-    struct AdfDevice * const device = adfDevOpen ( adfname, ADF_ACCESS_MODE_READWRITE );
+    struct AdfDevice * const device = adfDevOpen ( options.adfName,
+                                                   ADF_ACCESS_MODE_READWRITE );
     if ( device == NULL ) {
-        fprintf ( stderr, "Cannot open floppy disk %s - aborting...\n", adfname );
+        fprintf ( stderr, "Cannot open '%s' - aborting...\n", options.adfName );
         return 1;
     }
 
     ADF_RETCODE rc = adfDevMount ( device );
     if ( rc != ADF_RC_OK ) {
         fprintf ( stderr, "adfDevMount failed on %s - aborting...\n",
-                  adfname );
-        adfDevUnMount ( device );
+                  options.adfName );
         adfDevClose ( device );
-        return 1;
+        exit ( EXIT_FAILURE );
     }
-    if ( device->nVol > 0 && adfVolIsFsValid ( device->volList[0] ) ) {
-        fprintf ( stderr, "The floppy disk %s already contains a filesystem (%s)"
-                  " - aborting...\n", adfname, adfVolGetFsStr ( device->volList[0] ) );
-        adfDevUnMount ( device );
+
+    if ( options.verbose )
+        adfDevInfo ( device );
+
+    if ( device->nVol <= (int) options.volidx ) {
+        fprintf ( stderr, "Invalid volume index %u, %s contains %d volume%s.\n",
+                  options.volidx, options.adfName, device->nVol,
+                  device->nVol > 1 ? "s" : "");
         adfDevClose ( device );
+        exit ( EXIT_FAILURE );
+    }
+
+    if ( ! options.force ) {
+        if ( adfVolIsFsValid ( device->volList[ options.volidx ] ) ) {
+            fprintf ( stderr, "Volume %u of %s already contains a filesystem (%s)"
+                      " - aborting... (use -f to enforce formatting anyway)\n",
+                      options.volidx, options.adfName,
+                      adfVolGetFsStr ( device->volList[ options.volidx ] ) );
+            adfDevClose ( device );
+            exit ( EXIT_FAILURE );
+        }
+    }
+    adfDevUnMount ( device );
+
+    char *devtype_str;
+    if ( device->devType == ADF_DEVTYPE_FLOPDD ) {
+        devtype_str     = "DD floppy (880k)";
+    } else if ( device->devType == ADF_DEVTYPE_FLOPHD ) {
+        devtype_str     = "HD floppy (1760k)";
+    } else if ( device->devType == ADF_DEVTYPE_HARDFILE ) {
+        devtype_str     = "Hardfile (hdf)";
+    } else { //if ( devtype == DEVTYPE_HARDDISK ) {
+        fprintf ( stderr, "Devices with RDB (partitioned) are not supported "
+                  "(yet...) - aborting...\n" );
         return 1;
     }
 
+    printf ( "Formatting %s '%s', volume %d, DOS fstype %d, label '%s'... ",
+             devtype_str, options.adfName, options.volidx, options.fsType, options.label );
+    fflush ( stdout );
 
-    //adfDeviceInfo ( device );
-
-    char *fdtype;
-    int devtype = adfDevType ( device );
-    if ( devtype == ADF_DEVTYPE_FLOPDD ) {
-        device->sectors = 11;
-        device->heads   = 2;
-        fdtype          = "DD";
-    } else if ( devtype == ADF_DEVTYPE_FLOPHD ) {
-        device->sectors = 22;
-        device->heads   = 2;
-        fdtype          = "HD";
-    } else { //if ( devtype == ADF_DEVTYPE_HARDDISK ) {
-        fprintf ( stderr, "The device is not a floppy - aborting...\n" );
-        return 1;
-    }
-    device->cylinders = device->size / ( device->sectors * device->heads * 512 );
-
-    adfDevInfo ( device );
-
-    printf ( "Formatting floppy (%s) disk '%s'...\n", fdtype, adfname );
-    if ( adfCreateFlop ( device, label, (unsigned char) type ) != ADF_RC_OK ) {
-        fprintf ( stderr, "Error formatting the disk image '%s'!", adfname );
+    rc = ( device->devType == ADF_DEVTYPE_HARDFILE ) ?
+        adfCreateHdFile ( device, options.label, options.fsType ) :
+        adfCreateFlop   ( device, options.label, options.fsType );
+    if ( rc != ADF_RC_OK ) {
+        fprintf ( stderr, "Error formatting '%s'!", options.adfName );
         adfDevClose ( device );
         adfEnvCleanUp();
         return 1;
     }
     printf ( "Done!\n" );
- 
+
+    if ( options.verbose )
+        adfDevInfo ( device );
+
     adfDevClose ( device );
     adfEnvCleanUp();
 
@@ -89,13 +147,91 @@ int main ( int     argc,
 }
 
 
-void usage ( void )
+
+/* return value: true - valid, false - invalid */
+bool parse_args ( const int * const    argc,
+                  char * const * const argv,
+                  CmdlineOptions *     options )
 {
-    printf ( "Usage:  adf_floppy_format filename label fstype\n\n"
-//             " where:\n"
-             "   fstype can be 0-7: flags = 3 least significant bits\n"
-             "         bit  set         clr\n"
-             "         0    FFS         OFS\n"
-             "         1    INTL ONLY   NO_INTL ONLY\n"
-             "         2    DIRC&INTL   NO_DIRC&INTL\n\n" );
+    // set default options
+    memset ( options, 0, sizeof ( CmdlineOptions ) );
+    options->volidx = 0;
+    options->label   = "";
+    options->force   =
+    options->verbose =
+    options->help    =
+    options->version = false;
+
+    const char * valid_options = "l:p:t:fhvV";
+    int opt;
+    while ( ( opt = getopt ( *argc, (char * const *) argv, valid_options ) ) != -1 ) {
+//        printf ( "optind %d, opt %c, optarg %s\n", optind, ( char ) opt, optarg );
+        switch ( opt ) {
+        case 'f': {
+            options->force = true;
+            continue;
+        }
+        case 'l': {
+            options->label = optarg;
+            size_t labelLen = strlen ( options->label );
+            if ( labelLen > ADF_MAX_NAME_LEN ) {
+                fprintf ( stderr, "Label '%s' is too long (%ld > max. %d characters).\n",
+                          options->label, labelLen, ADF_MAX_NAME_LEN );
+                return false;
+            }
+            continue;
+        }
+
+        case 'p': {
+            // partition (volume) number (index)
+            char * endptr = NULL;
+            options->volidx = ( unsigned int ) strtoul ( optarg, &endptr, 10 );
+            if ( endptr == optarg ||
+                 options->volidx > 255 )  // is there a limit for max. number of partitions???
+            {
+                fprintf ( stderr, "Invalid volume/partition %u.\n", options->volidx );
+                return false;
+            }
+            continue;
+        }
+
+        case 't': {
+            // filesystem type (subtype - just the number id in 'DOSn' signature)
+            char * endptr = NULL;
+            options->fsType = ( uint8_t ) strtoul ( optarg, &endptr, 10 );
+            if ( endptr == optarg ||
+                 options->fsType > 7 ||
+                 ( options->fsType > 0 &&
+                   options->fsType % 2 == 0 ) )
+            {
+                fprintf ( stderr, "Invalid filesystem type %u.\n", options->fsType );
+                return false;
+            }
+            continue;
+        }
+
+        case 'v':
+            options->verbose = true;
+            continue;
+
+        case 'h':
+            options->help = true;
+            return true;
+
+        case 'V':
+            options->version = true;
+            return true;
+
+        default:
+            return false;
+        }
+    }
+
+    if ( optind != *argc - 1 ) {
+        fprintf ( stderr, "Missing the name of an adf file/device.\n" );
+        return false;
+    }
+
+    options->adfName = argv [ optind ];
+    return true;
 }
