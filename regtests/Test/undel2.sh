5d78bd9a19871b568cedf78785f0695de2e64734
t-m <tomas.wolak@gmail.com>
2024-03-20 16:22:11 +0100

    regtests: merge undel3 test into undel2.
    The undel3 test differed from undel2 only in 2 things:
    the adf device (OFS vs FFS) used and the file to delete/recover
    (its name and sector). After improving undel2 (prev. commits),
    merging them together became possible and an obvious improvement.

--- regtests/Test/undel2.sh
+++ regtests/Test/undel2.sh
@@ -11,3 +11,10 @@ cat $FFSDUMP >testffs_adf
 ./undel2 testffs_adf ${TEST_FILE} 886
 #diff mod.distant $CHECK/mod.And.DistantCall
 rm -v ${TEST_FILE} testffs_adf
+
+#echo "----- undel3"
+TEST_FILE=MOON.GIF
+cat $OFSDUMP >testofs_adf
+./undel2 testofs_adf ${TEST_FILE} 884
+#diff moon_gif $CHECK/MOON.GIF
+rm -v ${TEST_FILE} testofs_adf
